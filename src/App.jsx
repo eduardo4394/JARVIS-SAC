db03f2a126852c4949b67f03a222d0d929d6fde2
eduardo salinas <eduardo_sh@outlook.com>
2023-01-30 10:57:30 -0500

    add function that gets data from users

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,56 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { getUsers } from "./services/Users";
+import styled from "@emotion/styled";
+
+const InfoUser = styled.div`
+  background-color: #6f6fd5;
+  border: 1px solid black;
+  color: white;
+  padding: 0.5rem;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+`;
 
 function App() {
-  const [users, setUsers] = useState("");
+  const [users, setUsers] = useState([]);
 
-  // const testData = getUsers();
+  const settingData = async () => {
+    const data = await getUsers();
+    setUsers(data.results);
+  };
 
   useEffect(() => {
-    const data = getUsers();
-    setUsers(data);
+    settingData();
   }, []);
+
+  const orderedUsers = [...users].sort((a, b) => a.dob.age - b.dob.age);
+  console.log(orderedUsers);
 
   return (
     <div className="App">
       <h1>Home</h1>
-      {/* {users &&
-        users.map((data) => {
-          return <p>{data.name.first}</p>;
-        })} */}
+      <InfoUser>
+        <p>Nombre</p>
+        <p>Apellido</p>
+        <p>Edad</p>
+        <p>Email</p>
+        <p>Nacionalidad</p>
+        <p>Foto</p>
+      </InfoUser>
+      {users &&
+        orderedUsers.map((user) => (
+          <InfoUser key={user.email}>
+            <p>{user.name.first}</p>
+            <p>{user.name.last}</p>
+            <p>{user.dob.age}</p>
+            <p>{user.email}</p>
+            <p>{user.nat}</p>
+            <img src={user.picture.thumbnail} alt="" />
+          </InfoUser>
+        ))}
     </div>
   );
 }
--- src/services/Users.jsx
+++ src/services/Users.jsx
@@ -1,6 +1,9 @@
 export const getUsers = async () => {
-  const res = await fetch("https://randomuser.me/api/?results=15");
-  const data = await res.json();
-  console.log(data);
-  return data;
+  try {
+    const res = await fetch("https://randomuser.me/api/?results=15");
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log("there was an error: " + error.message);
+  }
 };
